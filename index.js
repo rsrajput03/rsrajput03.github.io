1c16c874a789a4d1119bd60280da1b5a260b2294
rsrajput03 <rajputrohit9560@gmail.com>
2023-02-12 15:09:36 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,27 @@
+let navMenu = document.querySelector("#nav-menu");
+let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", mobile);
+
+function mobile() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+let navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", close));
+
+function close() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+GitHubCalendar(".react-activity-calendar", "rsrajput03", { responsive: true });
+
+document.getElementById("resume-button-2").onclick = () => {
+  location.href = "\\static\\pdf\\Rohit-Rajput-Resume.pdf";
+};
+document.getElementById("resume-button-1").onclick = () => {
+  location.href = "\\static\\pdf\\Rohit-Rajput-Resume.pdf";
+};
